0297a352629d29c0487179387cb17e73c90c6bf9
Alan Boglioli <alan.boglioli@gmail.com>
2017-06-26 20:04:47 -0300

    Add product unit test

---
+++ tests/products.spec.js
@@ -0,0 +1,22 @@
+const { expect } = require('chai');
+const request = require('supertest');
+
+const config = require('../src/config');
+const [_, server] = require('../index');
+
+describe('Products', () => {
+  before((done) => {
+    done();
+  });
+
+	it('GET /products', (done) => {
+    request(server.listener)
+      .get('/api/v1/product')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err ,res) => {
+        done(err);
+      });
+	});
+
+});
