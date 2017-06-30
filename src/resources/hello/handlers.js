30c55cb527c71d692eda76bf56be4608952c70a0
Alan <alan.boglioli@gmail.com>
2017-06-30 13:42:31 -0300

    Remove hello resource

--- src/resources/hello/handlers.js
+++
@@ -1,9 +0,0 @@
-class HelloHandler {
-  static async get(request, reply) {
-    return reply({
-      hello: 'world'
-    });
-  }
-}
-
-module.exports = HelloHandler;
--- src/routes.js
+++ src/routes.js
@@ -3,18 +3,16 @@ const url = require('url');
 const config = require('./config');
 const { buildRoutes } = require('./utils/routes');
 
-const hello = require('./resources/hello/routes');
 const setup = require('./resources/setup/routes');
 const users = require('./resources/users/routes');
 const account = require('./resources/account/routes');
-const product = require('./resources/products/routes');
+const products = require('./resources/products/routes');
 
 const routes = {
-  hello,
   users,
   account,
   setup,
-  product
+  products
 };
 
 module.exports = buildRoutes(config.app.routes, routes);
